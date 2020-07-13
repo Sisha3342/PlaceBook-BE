993d516b49f093c2ca8fbbb828a32671fef95cc7
AZelikov <andrey.zelikay@gmail.com>
2020-07-13 19:08:29 +0300

    PB-8: project is set up

---
+++ PlaceBook-BE/place-book/src/main/java/com/exadel/placebook/PlaceBookApplication.java
@@ -0,0 +1,13 @@
+package com.exadel.placebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaceBookApplication.class, args);
+    }
+
+}
---
+++ PlaceBook-BE/place-book/src/test/java/com/exadel/placebook/PlaceBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exadel.placebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
