5eeb3afdb783ec61a85f28aae8d36d3db2c1e25e
Vlad <vladicus7777720@mail.ru>
2020-07-17 14:03:13 +0300

    Create RestExceptionHandler (Fixed)

--- placebook-web/src/main/java/com/exadel/placebook/controller/HelloWorldController.java
+++ placebook-web/src/main/java/com/exadel/placebook/controller/HelloWorldController.java
@@ -1,7 +1,7 @@
 package com.exadel.placebook.controller;
 
 
-import com.exadel.placebook.exception.ErrorResponseException;
+import com.exadel.placebook.exception.ApplicationRuntimeException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/hello")
     public String helloWorld() throws Exception{
-        if(true) throw new ErrorResponseException("hello im exception");
         return "Hello world!";
+    }
+    @GetMapping("/status")
+    public String StatusController () throws Exception{
+        return "Application is running!";
     }
 }
--- placebook-web/src/main/java/com/exadel/placebook/exception/GlobalExceptionHandler.java
+++ placebook-web/src/main/java/com/exadel/placebook/exception/GlobalExceptionHandler.java
@@ -4,11 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
-    @ExceptionHandler(value = {ErrorResponseException.class})
-    public ResponseEntity<Object>  handleRestException(ErrorResponseException e){
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value = {ApplicationRuntimeException.class})
+    public ResponseEntity<?>  handleRestException(ApplicationRuntimeException e){
         final HttpStatus badRequest = HttpStatus.INTERNAL_SERVER_ERROR;
         ErrorResponse errorResponse =new ErrorResponse(
                 e.getMessage(),
@@ -17,7 +18,7 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponse, badRequest);
     }
     @ExceptionHandler(value={Exception.class})
-    public ResponseEntity<Object>  handleRestException(Exception e){
+    public ResponseEntity<?>  handleRestException(Exception e){
         final HttpStatus badRequest = HttpStatus.INTERNAL_SERVER_ERROR;
         ErrorResponse errorResponse =new ErrorResponse(
                 e.getMessage(),
