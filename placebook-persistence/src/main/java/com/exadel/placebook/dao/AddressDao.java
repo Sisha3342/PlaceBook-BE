10b340214f95ddc9593030659c82420c7ad3b0d6
cryztal <cryztal134@gmail.com>
2020-07-20 00:42:07 +0300

    Add Booking Info dialog add dao for small tables like Country, City, Address add entities for this tables and some small changes Add user information in bookingInfoController responce Add some methods to dao, which responce all cities, countries and address

--- placebook-persistence/src/main/java/com/exadel/placebook/dao/AddressDao.java
+++ placebook-persistence/src/main/java/com/exadel/placebook/dao/AddressDao.java
@@ -3,8 +3,11 @@ package com.exadel.placebook.dao;
 import com.exadel.placebook.model.entity.Address;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AddressDao extends CrudRepository<Address, Long> {
     Optional<Address> findById(Long id);
+
+    List<Address> findAll();
 }
--- placebook-persistence/src/main/java/com/exadel/placebook/dao/CountryDao.java
+++ placebook-persistence/src/main/java/com/exadel/placebook/dao/CountryDao.java
@@ -3,8 +3,10 @@ package com.exadel.placebook.dao;
 import com.exadel.placebook.model.entity.Country;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CountryDao extends CrudRepository<Country, Long> {
     Optional<Country> findById(Long id);
+    List<Country> findAll();
 }
