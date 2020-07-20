d86213151852c96c5dbb0a6d59c8a9307c84b64f
AZelikov <andrey.zelikay@gmail.com>
2020-07-20 15:00:04 +0300

    PB-9: success auth response.

--- placebook-web/src/main/java/com/exadel/placebook/sequrity/handler_impl/AuthSuccessHandlerImpl.java
+++ placebook-web/src/main/java/com/exadel/placebook/sequrity/handler_impl/AuthSuccessHandlerImpl.java
@@ -1,19 +1,27 @@
 package com.exadel.placebook.sequrity.handler_impl;
 
-import org.springframework.http.HttpStatus;
+import com.exadel.placebook.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 public class AuthSuccessHandlerImpl implements AuthenticationSuccessHandler {
+
+    @Autowired
+    UserService userService;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
                                         HttpServletResponse httpServletResponse,
-                                        Authentication authentication) {
-        httpServletResponse.setStatus(HttpStatus.OK.value());
+                                        Authentication authentication) throws IOException {
+
+        httpServletResponse.getWriter().write(new ObjectMapper().writeValueAsString(userService.getUserStatus()));
     }
 }
