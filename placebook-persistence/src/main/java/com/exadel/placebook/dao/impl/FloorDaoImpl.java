464a1152b762338ab5d38dc3aa9a883cc2daf7e7
AZelikov <andrey.zelikay@gmail.com>
2020-07-21 18:37:52 +0300

    PB-28: hibernate annotations; dao; dto

---
+++ placebook-persistence/src/main/java/com/exadel/placebook/dao/impl/FloorDaoImpl.java
@@ -0,0 +1,7 @@
+package com.exadel.placebook.dao.impl;
+
+import com.exadel.placebook.dao.FloorDao;
+import com.exadel.placebook.model.entity.Floor;
+
+public class FloorDaoImpl extends BaseDaoImpl<Floor> implements FloorDao {
+}
---
+++ placebook-persistence/src/main/java/com/exadel/placebook/dao/impl/OfficeDaoImpl.java
@@ -0,0 +1,7 @@
+package com.exadel.placebook.dao.impl;
+
+import com.exadel.placebook.dao.OfficeDao;
+import com.exadel.placebook.model.entity.Office;
+
+public class OfficeDaoImpl extends BaseDaoImpl<Office> implements OfficeDao {
+}
