f29641564a343e82658589027ffea02d8d3e773c
Vlad <vladicus7777720@mail.ru>
2020-07-29 20:00:20 +0300

    PB-58: Add API to allow HR get all his employees (Admin take all bookings filtered only by status)

--- placebook-service/src/main/java/com/exadel/placebook/service/BookingService.java
+++ placebook-service/src/main/java/com/exadel/placebook/service/BookingService.java
@@ -11,6 +11,8 @@ import java.util.Map;
 public interface BookingService {
 
     List<BookingDto> findByStatus(Long id, Status status);
+    List<BookingDto> yemployeesBookingsByStatusAndHrId(Status status);
+    List<BookingDto> yemployeesBookingsByStatus(Status status);
     List<BookingDto> findBookings(Long userId);
     List<PlaceHistoryDto> findPlaceHistory(Long placeId, LocalDateTime timeStart, LocalDateTime timeEnd);
     Map<Status, Long> getStatistics(Long id);
