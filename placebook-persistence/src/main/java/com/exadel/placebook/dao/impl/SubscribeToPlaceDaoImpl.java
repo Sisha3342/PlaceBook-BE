3e33e7a8fa56b2b0db1922fbad10ad871450cd33
Alena <Nat_030178@mail.ru>
2020-08-05 01:38:56 +0300

    fix dao

--- placebook-persistence/src/main/java/com/exadel/placebook/dao/impl/SubscribeToPlaceDaoImpl.java
+++ placebook-persistence/src/main/java/com/exadel/placebook/dao/impl/SubscribeToPlaceDaoImpl.java
@@ -22,10 +22,9 @@ public class SubscribeToPlaceDaoImpl extends BaseDaoImpl<Subscribe> implements S
         List<Status> status = new ArrayList<>();
         status.add(Status.CANCELED);
         status.add(Status.COMPLETED);
-        return session.createQuery("select distinct s " +
+        return session.createQuery("select s " +
                 "from Subscribe s " +
-                "join s.place p "+
-                "join p.bookings b "+
+                "join Booking b on b.place.id = s.place.id and b.user.id =s.user.id "+
                 "where b.status IN :status"
                         , Subscribe.class)
                 .setParameter("status", status)
