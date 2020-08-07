a71e2a0340dcb0d4e0e02adcc621654193ba6b5b
AZelikov <andrey.zelikay@gmail.com>
2020-08-07 22:58:48 +0300

    Get places refactored

--- placebook-persistence/src/main/java/com/exadel/placebook/dao/PlaceDao.java
+++ placebook-persistence/src/main/java/com/exadel/placebook/dao/PlaceDao.java
@@ -5,6 +5,7 @@ import com.exadel.placebook.model.entity.Place;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 public interface PlaceDao extends BaseDao<Place> {
     List<PlaceSearchDto> getPlaceByUserNow(Long userId);
@@ -12,5 +13,7 @@ public interface PlaceDao extends BaseDao<Place> {
     List<Place> findPlacesByFloorId(Long floorId);
 
     List<Place> getFreePlacesByFloorIdAndTimeRange(Long floorId, LocalDateTime start, LocalDateTime end);
+
+    Map<Place, Boolean> getPlacesWithOccupation(Long floorId, LocalDateTime timeStart, LocalDateTime timeEnd);
 }
 
