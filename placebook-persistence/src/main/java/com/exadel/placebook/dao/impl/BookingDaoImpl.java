ef1fd9e876db7595ea9ce5a7d31c6206bcd8c0fe
Vlad <vladicus7777720@mail.ru>
2020-08-17 17:07:58 +0300

    Fix BookingDaoImpl, add to DAO office.deleted=false

--- placebook-persistence/src/main/java/com/exadel/placebook/dao/impl/BookingDaoImpl.java
+++ placebook-persistence/src/main/java/com/exadel/placebook/dao/impl/BookingDaoImpl.java
@@ -53,7 +53,8 @@ public class BookingDaoImpl extends BaseDaoImpl<Booking> implements BookingDao {
                 "left join fetch f.office o " +
                 "left join fetch o.address a " +
                 "where u.id = :user_id and " +
-                "b.status = :status order by " + table + " " + order;
+                "b.status = :status and " +
+                "o.deleted = false order by " + table + " " + order;
         return session
                 .createQuery(queryStr, Booking.class)
                 .setParameter("user_id", userId)
@@ -74,7 +75,7 @@ public class BookingDaoImpl extends BaseDaoImpl<Booking> implements BookingDao {
                         "left join fetch b.user u " +
                         "left join fetch p.floor f " +
                         "left join fetch f.office off " +
-                        "left join fetch off.address ad where b.status = :status " +
+                        "left join fetch off.address ad where b.status = :status and o.deleted = false " +
                         "order by " + table + " " + order, Booking.class)
                 .setParameter("status", bookingSorting.getStatus())
                 .setMaxResults(bookingSorting.getLimit())
@@ -93,8 +94,8 @@ public class BookingDaoImpl extends BaseDaoImpl<Booking> implements BookingDao {
                         "left join fetch b.user u " +
                         "left join fetch p.floor f " +
                         "left join fetch f.office off " +
-                        "left join fetch off.address ad where u.hrId = :hrId " +
-                        "and b.status = :status" +
+                        "left join fetch off.address ad where u.hrId = :hrId and o.deleted = false " +
+                        "and b.status = :status " +
                         "order by " + table + " " + order, Booking.class)
                 .setParameter("hrId", id)
                 .setParameter("status", bookingSorting.getStatus())
@@ -114,7 +115,7 @@ public class BookingDaoImpl extends BaseDaoImpl<Booking> implements BookingDao {
                         "left join fetch b.user u " +
                         "left join fetch p.floor f " +
                         "left join fetch f.office off " +
-                        "left join fetch off.address ad where u.id = :user_id " +
+                        "left join fetch off.address ad where u.id = :user_id and o.deleted = false " +
                         "order by " + table + " " + order, Booking.class)
                 .setParameter("user_id", userId)
                 .setMaxResults(bookingSorting.getLimit())
